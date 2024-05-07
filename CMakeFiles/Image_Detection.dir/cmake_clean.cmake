98efaa8409c4bf66b31b26e421f5904de46f82ac
ManoharK <hellomanoharkr@gmail.com>
2024-05-07 11:18:13 +0530

    object detection from image

---
+++ CMakeFiles/Image_Detection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image_Detection.dir/Image_Detection.cpp.o"
+  "CMakeFiles/Image_Detection.dir/Image_Detection.cpp.o.d"
+  "Image_Detection"
+  "Image_Detection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image_Detection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
